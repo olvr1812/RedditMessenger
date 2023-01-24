ae9d8777d50e1d0557ad974d79c2fc162e2c733d
GeorgeTheM3 <99905582+GeorgeTheM3@users.noreply.github.com>
2023-01-24 19:39:52 +0300

    added main fonts and colors

--- RedditMessenger/Helper/Constants.swift
+++ RedditMessenger/Helper/Constants.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import UIKit
+
+class Constants {
+    //app colors
+    static let appColorOrange: UIColor = UIColor(red: 1, green: 119/255, blue: 0, alpha: 1)
+    static let appColorEasyGray: UIColor = .systemGray6
+    static let appColorMediumGray: UIColor = .systemGray3
+    static let appTintColor: UIColor = .black
+    
+    //app fonts
+    static let titleFont = UIFont(name: "AmericanTypewriter-CondensedBold", size: 20)
+    static let headlineFont = UIFont(name: "AmericanTypewriter", size: 17)
+    static let bodyFont = UIFont(name: "AmericanTypewriter", size: 15)
+    static let footnoteStatusFont = UIFont(name: "AmericanTypewriter-Bold", size: 16)
+}
