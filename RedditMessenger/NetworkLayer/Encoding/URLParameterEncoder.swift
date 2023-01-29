ca051659620056aebcc8d67d39af74c1efb8c661
GeorgeTheM3 <99905582+GeorgeTheM3@users.noreply.github.com>
2023-01-29 10:41:26 +0300

    create encoding

---
+++ RedditMessenger/NetworkLayer/Encoding/URLParameterEncoder.swift
@@ -0,0 +1,27 @@
+//
+//  URLParameterEncoder.swift
+//  RedditMessenger
+//
+//  Created by Георгий Матченко on 29.01.2023.
+//
+
+import Foundation
+
+public struct URLParameterEncoder: ParameterEncoderProtocol {
+    public static func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws {
+        guard let url = urlRequest.url else { throw NetworkError.missingURL }
+        
+        if var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false), !parameters.isEmpty {
+            urlComponents.queryItems = [URLQueryItem]()
+            for (key, value) in parameters {
+                let queryItem = URLQueryItem(name: key, value: "\(value)".addingPercentEncoding(withAllowedCharacters: .urlHostAllowed))
+                urlComponents.queryItems?.append(queryItem)
+            }
+            urlRequest.url = urlComponents.url
+        }
+        
+        if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
+            urlRequest.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        }
+    }
+}
