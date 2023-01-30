e115f36b1c6dce41b4d1152b40c07f2a5e787b9b
GeorgeTheM3 <99905582+GeorgeTheM3@users.noreply.github.com>
2023-01-30 20:08:26 +0300

    constants pull

---
+++ RedditMessenger/Helper/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  RedditMessenger
+//
+//  Created by Георгий Матченко on 30.01.2023.
+//
+
+import Foundation
+import UIKit
+
+class Constants {
+    //app colors
+    static let appColorOrange: UIColor = UIColor(red: 1, green: 119/255, blue: 0, alpha: 1)
+    static let appColorEasyGray: UIColor = .systemGray6
+    static let appColorMediumGray: UIColor = .systemGray3
+    static let appTintColor: UIColor = .black
+    
+    //app fonts
+    static let titleFont = UIFont(name: "AmericanTypewriter-CondensedBold", size: 20)
+    static let headlineFont = UIFont(name: "AmericanTypewriter", size: 17)
+    static let bodyFont = UIFont(name: "AmericanTypewriter", size: 15)
+    static let footnoteStatusFont = UIFont(name: "AmericanTypewriter-Bold", size: 16)
+}
